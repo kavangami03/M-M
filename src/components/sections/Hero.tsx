0342a29db5025ef7e7911d82c9545f97600311a0
Kavan Gami <kavan.gami@softqubes.com>
2026-06-01 19:16:29 +0530

    Design Changes Done

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
           alt="Security Operations Control Room Background"
           fill
           priority
-          className="object-fill"
+          className="object-cover object-bottom"
         />
       </div>
 
